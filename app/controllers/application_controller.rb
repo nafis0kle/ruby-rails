802cb7c769251649410b1b82576aab547c1e609e
Nafis <khusnutdinov.nafis@bk.ru>
2022-09-20 15:23:15 +0300

    Chapter #1 tasks are complete. Change text for hello and add goodbye function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render plain: "hello, world!"
+    render plain: "¡hola, mundo!"
+  end
+
+  def goodbye
+    render plain: "Goodbye, rails!"
   end
 end
